f7c7ba27fdbad42a33a7f843ab602699b6bc54a5
andrewkreuzer <me@andrewkreuzer.com>
2025-10-13 12:40:01 -0400

    fix: keygen

--- src/bin/agez-keygen.zig
+++ src/bin/agez-keygen.zig
@@ -1,10 +1,13 @@
 const std = @import("std");
+const assert = std.debug.assert;
+const crypto = std.crypto;
 const exit = std.posix.exit;
+const fs = std.fs;
 const Io = std.Io;
+const mem = std.mem;
+const dh = crypto.dh;
 const time = std.time;
 const Allocator = std.mem.Allocator;
-const ArgIterator = std.process.ArgIterator;
-const ArrayList = std.ArrayList;
 const File = std.fs.File;
 
 const argz = @import("argz");
@@ -14,10 +17,10 @@ const agez = @import("agez");
 const X25519 = agez.X25519;
 const bech32 = agez.bech32;
 
-const Args2 = struct {
-    help: argz.Arg(bool) = .{ .description = "Prints the help text" },
+const Args = struct {
+    input: argz.Positional(?String) = .{ .description = "Input private key from a path INPUT" },
     output: argz.Arg(?String) = .{ .description = "Output private key to a path OUTPUT" },
-    convert: argz.Arg(?String) = .{ .description = "Convert an identity file to a recipients file" },
+    convert: argz.Arg(bool) = .{ .short = "-y", .description = "Convert an identity file to a recipients file" },
 };
 
 pub fn main() !void {
@@ -28,59 +31,79 @@ pub fn main() !void {
     defer if (gpa.deinit() == .leak) { std.debug.print("Leak detected\n", .{}); };
     defer arena.deinit();
 
-    var cli = argz.Parse(Args2).init(allocator);
+    var cli = argz.Parse(Args).init(allocator);
     defer cli.deinit();
-
     const args = try cli.parse();
-    if (args.convert) |convert| {
-        var file = try std.fs.cwd().openFile(convert.inner, .{.mode = .read_write});
-        var reader_buf: [4096]u8 = undefined;
-        var reader: File.Reader = file.reader(&reader_buf);
-        var writer_buf: [4096]u8 = undefined;
-        var writer: File.Writer = file.writer(&writer_buf);
 
-        var recipients: std.ArrayList([]u8) = .empty;
-        defer {
-            for (recipients.items) |r| allocator.free(r);
-            recipients.deinit(allocator);
+    const stdin_file: File = .stdin();
+    const stdout_file: File = .stdout();
+    var in: File = if (args.input) |path|
+        try fs.cwd().openFile(path.inner, .{}) else stdin_file;
+
+    var out: File = if (args.output) |path|
+        try fs.cwd().createFile(path.inner, .{.truncate = true, .mode = 0o600}) else stdout_file;
+
+
+    var stdout_reader: File.Writer = stdout_file.writer(&.{});
+    var stdout: *Io.Writer = &stdout_reader.interface;
+
+    var in_reader_buf: [1024]u8 = undefined;
+    var in_reader: File.Reader = in.reader(&in_reader_buf);
+    var reader: *Io.Reader = &in_reader.interface;
+
+    var out_writer_buf: [1024]u8 = undefined;
+    var out_writer: File.Writer = out.writer(&out_writer_buf);
+    var writer: *Io.Writer = &out_writer.interface;
+
+    if (args.output != null and args.convert) {
+        try stdout.print("Cannot use --output and --convert together\n", .{});
+        exit(1);
+    }
+
+    if (args.output == null and !args.convert) {
+        try cli.printHelp();
+        exit(0);
+    }
+
+    if (args.convert) {
+        while (reader.takeDelimiterExclusive('\n')) |line| {
+            if (mem.startsWith(u8, line, "#")) continue;
+            var n: usize = 0;
+            var buf: [512]u8 = undefined;
+            defer crypto.secureZero(u8, &buf);
+            var w: Io.Writer = .fixed(&buf);
+
+            const id_b32 = try bech32.decode(try w.writableSlice(90), X25519.BECH32_HRP_PRIVATE, line);
+            const bits = try w.writableSlice(32);
+            n = try bech32.convertBits(bits, id_b32.data, 5, 8, false);
+            assert(n == 32);
+
+            const pk = try dh.X25519.recoverPublicKey(bits[0..32].*);
+            const bits_recipient = try w.writableSlice(90);
+            n = try bech32.convertBits(bits_recipient, &pk, 8, 5, true);
+            assert(n > 0);
+
+            const recipient = try w.writableSlice(90);
+            const recipient_b32 = try bech32.encode(recipient, "age", bits_recipient[0..n]);
+            try writer.writeAll(recipient_b32);
+            try writer.writeByte('\n');
+        } else |err| switch (err) {
+            error.EndOfStream => {},
+            else => |e| return e,
         }
+    }
 
-        var buf_line: [90]u8 = undefined;
-        var line_writer: Io.Writer = .fixed(&buf_line);
-        while (true) {
-            var buf_id: [90]u8 = undefined;
-            var buf_bytes: [90]u8 = undefined;
-            var buf_recipient: [90]u8 = undefined;
-            var buf_recipient_b32: [90]u8 = undefined;
-
-            _ = try reader.interface.streamDelimiter(&line_writer, '\n');
-            const b32 = try bech32.decode(&buf_id, X25519.BECH32_HRP_PRIVATE, line_writer.buffered());
-            _ = try bech32.convertBits(&buf_bytes, b32.data, 5, 8, false);
-
-            const pk = try std.crypto.dh.X25519.recoverPublicKey(buf_bytes[0..32].*);
-            const n = try bech32.convertBits(&buf_recipient, &pk, 8, 5, true);
-            const recipient_bech32 = try bech32.encode(&buf_recipient_b32, "age", buf_recipient[0..n]);
-            try recipients.append(allocator, try allocator.dupe(u8, recipient_bech32));
-        }
-
-        for (recipients.items) |r| {
-            try writer.interface.writeAll(r);
-            try writer.interface.writeByte('\n');
-        }
-
-        writer.interface.flush();
-        file.close();
-    } else {
+    if (args.output) |_| {
         var secret: [32]u8 = undefined;
-        defer std.crypto.secureZero(u8, &secret);
+        defer crypto.secureZero(u8, &secret);
 
         var recipient_bech32_buf: [90]u8 = undefined;
-        defer std.crypto.secureZero(u8, &recipient_bech32_buf);
+        defer crypto.secureZero(u8, &recipient_bech32_buf);
 
         var identity_bech32_buf: [90]u8 = undefined;
-        defer std.crypto.secureZero(u8, &identity_bech32_buf);
+        defer crypto.secureZero(u8, &identity_bech32_buf);
 
-        std.crypto.random.bytes(&secret);
+        crypto.random.bytes(&secret);
 
         const recipient = try std.crypto.dh.X25519.recoverPublicKey(secret);
 
@@ -94,25 +117,20 @@ pub fn main() !void {
 
         var buf_upper: [90]u8 = undefined;
         const id = std.ascii.upperString(&buf_upper, identity_bech32);
-        std.debug.print("Public key: {s}\n", .{recipient_bech32});
-        if (args.output) |output| {
-            var file = try std.fs.cwd().createFile(output.inner, .{.mode = 0o644});
-            var writer_buf: [128]u8 = undefined;
-            const file_writer = file.writer(&writer_buf);
-            var writer: Io.Writer = file_writer.interface;
-            try writer.writeAll("# created: ");
-            try writer.writeAll(&try ts());
-            try writer.writeAll("\n");
-            try writer.writeAll("# public key: ");
-            try writer.writeAll(recipient_bech32);
-            try writer.writeAll("\n");
-            try writer.writeAll(id);
-            try writer.writeAll("\n");
-            try writer.flush();
-        } else {
-            std.debug.print("{s}\n", .{id});
-        }
+        try stdout.print("Public key: {s}\n", .{recipient_bech32});
+
+        try writer.print(
+            \\# created: {s}
+            \\# public key: {s}
+            \\{s}
+            \\
+            , .{ try ts(), recipient_bech32, id }
+        );
     }
+
+    try writer.flush();
+    in.close();
+    out.close();
 
 }
 
